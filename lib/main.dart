fa4f71cb9562a0a97471c4aa746cfbf1ef1b5000
wiratzy <wirantochill6@gmail.com>
2024-06-06 13:58:15 +0700

    Belajar Text style dan perbedaan antara column dan listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DesainTeks());
+  runApp(const ListApp());
 }
+//latihan list view
+
+class ListApp extends StatefulWidget {
+  const ListApp({super.key});
+
+  @override
+  State<ListApp> createState() => _ListAppState();
+}
+
+class _ListAppState extends State<ListApp> {
+  List<Widget> widgets = [];
+  int counter = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan List View"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        widgets.add(Text(
+                          "Data Ke - " + counter.toString(),
+                          style: TextStyle(fontSize: 35),
+                        ));
+                        counter++;
+                      });
+                    },
+                    child: Text("Tambah Data")),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        widgets.removeLast();
+                        counter--;
+                      });
+                    },
+                    child: Text("Hapus Data"))
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widgets,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//latihan list view
 
 //Latian menggunakan widget TextStyle()
 class DesainTeks extends StatelessWidget {
@@ -18,7 +76,15 @@ class DesainTeks extends StatelessWidget {
       // ignore: prefer_const_constructors
       body: Center(
         // ignore: prefer_const_constructors
-        child: Text("Ini Adalah Text"),
+        child: Text("Ini Adalah Text",
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+                fontFamily: "CrashLandingBB",
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.wavy)),
       ),
     ));
   }
